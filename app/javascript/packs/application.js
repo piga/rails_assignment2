e07719eec2b55657a48c7af4944c09d7ade3778a
Darko Vulin <darkopmfst@gmail.com>
2022-02-20 13:22:12 +0100

    bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
+
+import "../stylesheets/my_style"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
