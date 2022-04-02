fc215f9846ef0f1e120ac99a164184278414dedf
Darko Vulin <darkopmfst@gmail.com>
2022-04-02 19:20:52 +0200

    Index stranica je srežena, login modal je dobar sve radi ali flash obavjesti nisu dobre.

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -1,73 +1,82 @@
 require 'rails_helper'
 
 RSpec.feature "Sessions", type: :feature do
-  context "interakcija s Log In botunom na root stranici" do
-
+  context "provjeravam dali postoje elementi na stranici za prijavu korisnika" do
+    
     before(:each) do 
       visit oglas_path  
     end
 
     
-    scenario "pronalazim log in botun" do
-      expect(page).to have_content("Log In")
+    scenario "pronalazim link Prijava" do
+      expect(page).to have_content("Prijava")
+    end
+    
+    scenario "login modal je skriven" do
+      expect(page).to have_css(".modal", visible: :hidden)
     end
     
     
-    scenario "log in me vodi do stranice sa emailom" do 
-      click_on("Log In")   
-      expect(page).to have_content("Email")
+    scenario "klik na Prijava mi otvara modal" do 
+      click_on("Prijava")
+      expect(page).to have_css(".modal", visible: :visible)
     end
     
-    scenario "log in me vodi do stranice sa lozinkom" do 
-      click_on("Log In")   
-      expect(page).to have_content("Lozinka")
+    scenario "email polje u modalu ima sve potrebne atribute" do 
+      click_on("Prijava")
+      expect(page.has_field?("email", class: "form-control")).to eq true
+      expect(page.has_css?("#emailInput")).to eq true
+    end
+    
+    scenario "krivo ispunjavam login" do 
+      click_on("Prijava")   
+      expect(page).to have_content("Modal title")
     end
   end
   
-  context "neuspješno logiranje" do
+  context "nemožeš se prijaviti ako si unio" do
     before(:each) do
       User.create(email: "neki@email.com", password: "lozinka")
-      visit "/login"
+      visit oglas_path
+      click_on("Prijava")
     end
     
     after(:each) do
+      click_button "Ulogiraj se"
       expect(page).to have_content("Unijeli ste pogrešan email ili lozinku.")
     end
     
     it "krivi email" do
         fill_in "Email", with: "netocni@email.com"
         fill_in "Lozinka", with: "lozinka"
-        click_button "Log in"
     end
     
     it "netočna lozinku" do
         fill_in "Email", with: "neki@email.com"
         fill_in "Lozinka", with: "loza"
-        click_button "Log in"
     end
     
     it "nema lozinke" do
         fill_in "Email", with: "neki@email.com"
-        click_button "Log in"
     end
     
     it "nema emaila" do
         fill_in "Lozinka", with: "lozinka"
-        click_button "Log in"
     end
   end
   
-  context "uspješno logiranje" do
+  context "možeš se prijaviti ako su ti" do
     it "email i lozinka točni" do
       User.create(email: "neki@email.com", password: "lozinka")
-      visit "/login"
-      
+      visit oglas_path
+
+      click_on "Prijava"
       fill_in "Email", with: "neki@email.com"
       fill_in "Lozinka", with: "lozinka"
-      click_button "Log in"
-          
+      click_button "Ulogiraj se"
+
       expect(page).to have_content("Prijavili ste se!")
-      expect(page).to have_content("Svi oglasi")
+      expect(page.has_link?("Odjava", visible: :visible)).to eq true
     end
   end
 end
