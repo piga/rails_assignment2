106c91873066f5d321db010dbcf63316348f49f9
Darko Vulin <darkopmfst@gmail.com>
2022-04-27 16:24:49 +0200

    I mailer radi i linkovi u njemu samo što ne možemo stisnuti na oglas u emailu ako taj oglas nije u bazi.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    #begin
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         flash[:success] = "Prijavili ste se!" 
@@ -11,9 +10,6 @@ class SessionsController < ApplicationController
         flash[:warning] = "Unijeli ste pogrešan email ili lozinku."
         redirect_to root_url
       end
-    #rescue BCrypt::Errors::InvalidHash
-      #puts "da da"
-    #end
   end
   
   def destroy
